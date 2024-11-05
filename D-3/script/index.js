9bedc990ebea2be129e5da5813a2c60bd7bd21ea
soy325 <soy325@github.com>
2024-11-05 15:03:46 +0900

    수정

--- D-3/script/index.js
+++ D-3/script/index.js
@@ -1,3 +1,13 @@
+// menu
+$('li.main-menu').mouseenter(function(){
+    $('ul.sub-menu').stop().fadeIn()
+    $('header').addClass('on')
+})
+$('li.main-menu').mouseleave(function(){
+    $('ul.sub-menu').stop().fadeOut()
+    $('header').removeClass('on')
+})
+
 // slide
 $('.slide').eq(0).siblings().hide()
 let idx=0
@@ -9,3 +19,21 @@ setInterval(function(){
     $('.slide').fadeOut()
     $('.slide').eq(idx).fadeIn()
 },3000)
+
+// tab
+$('.tab-list li').click(function(){
+    $('.tab-list li').removeClass('on')
+    $(this).addClass('on')
+
+    let idx = $(this).index()
+    $('.tab').hide()
+    $('.tab').eq(idx).show()
+})
+
+// popup
+$('li.modal').click(function () {
+    $('.popup').fadeIn()
+})
+$('a.close').click(function () {
+    $('.popup').fadeOut()
+})
